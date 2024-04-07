f02f4f6dd88458db69e06ea060f84d3229541edf
Mxtxx <64033837+Mxtxx@users.noreply.github.com>
2024-04-07 16:37:45 +0200

    udated the api id and keys added comments

--- request.py
+++ request.py
@@ -1,11 +1,17 @@
+#importing the libraries requests for HTTP requests to web servers
+#importing APIKeys to easily use the API we want by only modifying the name 
 import requests
 import APIKeys 
-from APIkeys import remy_key_spoonacular, remy_key_edamam, matteo_key_spoonacular, matteo_key_edamam
+from APIkeys import remy_key_spoonacular, remy_key_edamam, matteo_key_spoonacular, matteo_key_edamam, remy_id_edamam, matteo_id_edamam
 
+
+#defining a function that will allow to search the API using a query (future ingredients) put in by the user and the parameters containing the information to access the API
+#checking if the request was successfull
+#return a json of the API response
 def search_recipes(query):
-  api_key = 
-  api_id = 
-  api_url1 = 
+  api_key1 = remy_key_spoonacular
+  api_key2 = remy_key_edamam
+  api_id2 = remy_id_edamam
   api_url1 = "https://api.spoonacular.com/recipes/findByIngredients"
   api_url2 = 'https://api.edamam.com/search'
   params = { 
@@ -19,7 +25,7 @@ def search_recipes(query):
   else:
     return None
   
-  
+
 spoonacular_key = remy_key_spoonacular
 url = 'https://api.spoonacular.com/recipes/findByIngredients'
 params = {
@@ -29,6 +35,3 @@ params = {
 
 response = requests.get(url, params=params)
 data_spoonacular = response.json()
-
-
-

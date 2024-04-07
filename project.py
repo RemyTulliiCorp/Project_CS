f111953f29b162bb8a269030daba03e296bf4368
Mxtxx <64033837+Mxtxx@users.noreply.github.com>
2024-04-07 11:27:40 +0200

    Update project.py

--- project.py
+++ project.py
@@ -1,3 +1,7 @@
 import streamlit as st
 st.title('Kitchenalchemy')
 st.write("woooooooob")
+
+API_Key = "21c9e00f44924b25abcc48a7c763649b"
+API_Url = "https://api.spoonacular.com/recipes/findByIngredients"
+

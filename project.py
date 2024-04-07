ea1bd3061be5f06b5977b6be17c0e58d575b0c43
Mxtxx <64033837+Mxtxx@users.noreply.github.com>
2024-04-07 16:22:34 +0200

    Update project.py

--- project.py
+++ project.py
@@ -2,6 +2,5 @@ import streamlit as st
 import APIKeys
 st.title('Kitchenalchemy')
 st.write("testt")
-API_Url1 = "https://api.spoonacular.com/recipes/findByIngredients"
-API_Url2 = 'https://api.edamam.com/search'
+
 

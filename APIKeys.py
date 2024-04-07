b7443081a0f40c8c2a656310f82b1c93f801d191
RemyTullii <166280992+RemyTullii@users.noreply.github.com>
2024-04-07 16:36:40 +0200

    id edamam added

--- APIKeys.py
+++ APIKeys.py
@@ -3,3 +3,4 @@ remy_key_edamam = '58943c2de8259119c8daa889e9a785e0'
 
 matteo_key_spoonacular = '21c9e00f44924b25abcc48a7c763649b'
 matteo_key_edamam = '74d231e662ad72772fa95f906197159a'
+remy_id_edamam = '72dbe36c'

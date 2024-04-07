783a660be4743e85fa28a7a2cd9ea7fa4e7567f7
Mxtxx <64033837+Mxtxx@users.noreply.github.com>
2024-04-07 15:55:58 +0200

    Update APIKeys.py

--- APIKeys.py
+++ APIKeys.py
@@ -1,4 +1,5 @@
 remy_key_spoonacular = 'dea30a57b1404a738393a1053ffc37ed'
 remy_key_edamam = '58943c2de8259119c8daa889e9a785e0'
 
-matteo_key_spoonacular = "21c9e00f44924b25abcc48a7c763649b"
+matteo_key_spoonacular = '21c9e00f44924b25abcc48a7c763649b'
+matteo_key_edamam = '74d231e662ad72772fa95f906197159a'

2281894d8e20808ec134ae2114405d8ef377416a
RemyTullii <166280992+RemyTullii@users.noreply.github.com>
2024-04-07 16:59:44 +0200

    dict

--- APIKeys.py
+++ APIKeys.py
@@ -1,16 +1,15 @@
-remy_key_spoonacular = 'dea30a57b1404a738393a1053ffc37ed'
-remy_key_edamam = '58943c2de8259119c8daa889e9a785e0'
-remy_id_edamam = '72dbe36c'
-
-matteo_key_spoonacular = '21c9e00f44924b25abcc48a7c763649b'
-matteo_key_edamam = '74d231e662ad72772fa95f906197159a'
-matteo_id_edamam = 'c1718ec0'
-
-nathan_key_spoonacular = 'input'
-nathan_key_edamam = 'input'
-nathan_id_key = 'input'
-
-lev_key_spoonacular = 'input'
-lev_key_edamam = 'input'
-lev_id_key = 'input'
+all_keys = {
+    'remy_key_spoonacular' : 'dea30a57b1404a738393a1053ffc37ed',
+    'remy_key_edamam' : '58943c2de8259119c8daa889e9a785e0',
+    'remy_id_edamam' : '72dbe36c',
+    'matteo_key_spoonacular': '21c9e00f44924b25abcc48a7c763649b',
+    'matteo_key_edamam': '74d231e662ad72772fa95f906197159a',
+    'matteo_id_edamam': 'c1718ec0',
+    'nathan_key_spoonacular' : 'input',
+    'nathan_key_edamam': 'input',
+    'nathan_id_key' : 'input',
+    'lev_key_spoonacular' : 'input',
+    'lev_key_edamam' : 'input',
+    'lev_id_key' : 'input',
+}  
 

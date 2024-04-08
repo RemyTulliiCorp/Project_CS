07739b9910f8150118b237599ed72fd8877b264e
RemyTullii <166280992+RemyTullii@users.noreply.github.com>
2024-04-08 20:26:23 +0200

    output

--- project.py
+++ project.py
@@ -5,6 +5,14 @@ from request import search_recipes
 st.title(':violet[KitchenAlchemy]')
 # User input for ingredients 
 your_ingredients = st.text_input('Enter the ingredients you have in your fridge, separated by a coma')
+# button to lauch the search
 output_recipes = st.button('Find Recipes')
+
+if output_recipes == True:
+    recipes = search_recipes(your_ingredients)
+    for i in recipes:
+                st.subheader(i['recipe']['label'])
+                st.image(i['recipe']['image'])
+
 
 

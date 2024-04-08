21180f0d3524e9c77080176800d621cd877e3330
RemyTullii <166280992+RemyTullii@users.noreply.github.com>
2024-04-08 20:17:38 +0200

    w

--- project.py
+++ project.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import requests
 from request import search_recipes
-st.title('Kitchenalchemy')
-your_ingredients = st.text_input('Enter the ingredients you have in your fridge')
+# Title
+st.title(':violet[KitchenAlchemy]')
+# User input for ingredients 
+your_ingredients = st.text_input('Enter the ingredients you have in your fridge, separated by a coma')
+output_recipes = st.button('Find Recipes')
+
 

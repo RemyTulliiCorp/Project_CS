b01e7918394a8945fcb3453f5225ee3fe2873083
RemyTullii <166280992+RemyTullii@users.noreply.github.com>
2024-04-08 20:04:50 +0200

    ws

--- project.py
+++ project.py
@@ -3,5 +3,4 @@ import requests
 from request import search_recipes
 st.title('Kitchenalchemy')
 your_ingredients = st.text_input('Enter the ingredients you have in your fridge')
-ingredients = search_recipes('pasta')
-st.write(ingredients)
+

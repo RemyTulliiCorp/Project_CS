80c36f089a3ca3fa62ebb87a7671d7365734870a
RemyTullii <166280992+RemyTullii@users.noreply.github.com>
2024-04-08 20:39:36 +0200

    new feature

--- project.py
+++ project.py
@@ -8,12 +8,19 @@ your_ingredients = st.text_input('Enter the ingredients you have in your fridge,
 # button to lauch the search
 output_recipes = st.button('Find Recipes')
 
-if output_recipes: # == if you press on the button
-    recipes = search_recipes(your_ingredients) 
-    for i in recipes:
-                st.subheader(i['recipe']['label'], divider = 'blue')
-                st.image(i['recipe']['image'])
-                for ingredient in i['recipe']['ingredientLines']:
-                    st.write(f"- {ingredient}")
+if output_recipes == True: # == if you press on the button
+    recipes = search_recipes(your_ingredients)
+    if recipes == True: # == if a recipe exists
+        for i in recipes:
+            st.subheader(i['recipe']['label'], divider = 'blue') # give the name of the recipe
+            st.image(i['recipe']['image']) # put an image of the recipe
+    else:
+        st.write('No recipe was found :hankey:')
+
+
+        
+
+        
+
 
 

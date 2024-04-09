79859e6136d40d4e8e0319408d756c28f8988ac7
RemyTullii <166280992+RemyTullii@users.noreply.github.com>
2024-04-09 10:05:39 +0200

    d

--- request.py
+++ request.py
@@ -2,7 +2,7 @@
 # importing APIKeys to easily use the API we want by only modifying the name 
 import requests
 from api_keys import all_keys
-
+from project import cuisine_type
 # defining a function that will allow to search the API using a query (future ingredients) put in by the user and the parameters containing the information to access the API
 # checking if the request was successfull
 # return a json of the API response
@@ -17,6 +17,9 @@ def search_recipes(your_ingredients):
     'app_id' : api_id2,
     'app_key' : api_key2
   }
+  if cuisine_type:
+    params['cuisineType'] = cuisine_type
+
   response = requests.get(api_url2, params = params)
   if response.status_code == 200:
     return response.json()

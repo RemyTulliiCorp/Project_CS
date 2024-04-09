ded92c736cf3519af20dd892d57c01aa5039f99b
NWillemin <154634084+NWillemin@users.noreply.github.com>
2024-04-09 14:39:24 +0200

    a

--- project.py
+++ project.py
@@ -22,7 +22,7 @@ if output_recipes: # == if you press on the button
             st.subheader(recipe['label']) # give the name of the recipe
             st.write(f' Total calories: {round(recipe["calories"])}') # calories 
             if recipe['totalTime']>0:
-                st.write(f' {recipe['totalTime']} minutes') # cooking time
+                st.write(f" {recipe['totalTime']} minutes") # cooking time
             for ingredient in recipe['ingredientLines']:
                 st.write(f'{ingredient}')
             url_recipe = recipe['url']

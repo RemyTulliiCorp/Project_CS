619acf8492f25e02a569aa22eac9f35ec1b02d00
NWillemin <154634084+NWillemin@users.noreply.github.com>
2024-04-09 14:41:11 +0200

    d

--- project.py
+++ project.py
@@ -7,10 +7,10 @@ st.title(':violet[KitchenAlchemy]')
 your_ingredients = st.text_input('Enter the ingredients you have in your fridge, separated by a coma')
 # button to select the cuisine type: chinese, italian, etc.
 cuisine_type = st.selectbox('Choose a cuisine type', ('', 'American', 'Asian', 'British', 'Caribbean', 'Central Europe', 'Chinese', 'Eastern Europe', 'French', 'Indian', 'Italian', 'Japanese', 'Kosher', 'Mediterranean', 'Mexican', 'Middle Eastern', 'Nordic', 'South American', 'South East Asian'), index=0)
-# button to lauch the search
-output_recipes = st.button('Find Recipes')
 # filter the maximum time period
 max_time = st.selectbox('Maximum total time in minutes', ('', '15', '30', '45'), index=0, key='time')
+# button to lauch the search
+output_recipes = st.button('Find Recipes')
 
 if output_recipes: # == if you press on the button
     data = search_recipes(your_ingredients, cuisine_type if cuisine_type else None, max_time if max_time else None) # calls the function

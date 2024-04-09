6e911ecff348d2879acd03d64dd48d1b268e3e68
NWillemin <154634084+NWillemin@users.noreply.github.com>
2024-04-09 11:34:02 +0200

    a

--- request.py
+++ request.py
@@ -20,7 +20,7 @@ def search_recipes(your_ingredients, cuisine_type = None, max_time = None):
   cuisine_type = st.selectbox('Choose a cuisine type', ('', 'American', 'Asian', 'British', 'Caribbean', 'Central Europe', 'Chinese', 'Eastern Europe', 'French', 'Indian', 'Italian', 'Japanese', 'Kosher', 'Mediterranean', 'Mexican', 'Middle Eastern', 'Nordic', 'South American', 'South East Asian'), index=0, key = 'type')
   if cuisine_type:
     params['cuisineType'] = cuisine_type
-  max_time = st.selectbox('Maximum total time in minutes', ('', '15', '30', '45'), key = 'time')
+  max_time = st.selectbox('Maximum total time in minutes', ('', '15', '30', '45'), index=0, key = 'time')
   if max_time:
     params['time'] = max_time
   response = requests.get(api_url2, params = params)

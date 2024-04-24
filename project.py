e408231b0939254f5aa11a3a9da78c05b08e00db
NWillemin <154634084+NWillemin@users.noreply.github.com>
2024-04-24 17:38:31 +0200

    a

--- project.py
+++ project.py
@@ -57,7 +57,7 @@ if output_recipes: # == if you press on the button
                 })
             chart_data.set_index('Nutrients', inplace=True)
             with st.expander("Show Nutrients"):
-                st.bar_chart(data=chart_data, y=['Macronutrients(g)', 'Details'])
+                st.bar_chart(data=chart_data, y=['Macronutrients (g)', 'Details'])
 
             for ingredient in recipe['ingredientLines']:# write the necessary ingredients for each recipe
                 st.write(f'{ingredient}')

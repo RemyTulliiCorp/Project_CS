6451d6cb5529ced36a955cf4c71387b01e236d63
RemyTullii <166280992+RemyTullii@users.noreply.github.com>
2024-05-11 20:20:58 +0200

    a

--- project.py
+++ project.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 from request import search_recipes
 import pandas as pd
-import mathplotlib.pyplot
+import matplotlib.pyplot as plt
 
 # Title
 st.title('Kitchen:red[Alchemy]')

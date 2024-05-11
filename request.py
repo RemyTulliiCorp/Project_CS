60f895b08bb7d66e6d15da5024c90ca7cf27bfec
Mxtxx <64033837+Mxtxx@users.noreply.github.com>
2024-05-11 17:21:45 +0200

    Update request.py
    Ajout d'explications

--- request.py
+++ request.py
@@ -3,15 +3,16 @@
 import requests
 from api_keys import all_keys
 import streamlit as st
+
 # defining a function that will allow to search the API using a query (future ingredients) put in by the user and the parameters containing the information to access the API
-# checking if the request was successfull
-# return a json of the API response
 def search_recipes(your_ingredients, cuisine_type = None, max_time = None, excluded_ingredients = None, max_calories = None):
-  api_key1 = all_keys['remy_key_spoonacular'] # change the name if limit exceeded
-  api_key2 = all_keys['matteo_key_edamam'] # change the name if limit exceeded
-  api_id2 = all_keys['matteo_id_edamam'] # change the name if limit exceeded
-  api_url1 = "https://api.spoonacular.com/recipes/extract"
+  api_key1 = all_keys['remy_key_spoonacular'] #setting the api keys to the respective keys as we created more than one account to have more free queries
+  api_key2 = all_keys['matteo_key_edamam']
+  api_id2 = all_keys['matteo_id_edamam']
+  api_url1 = "https://api.spoonacular.com/recipes/extract" #setting the two api urls for the query to go through to the api
   api_url2 = 'https://api.edamam.com/search'
+
+  #defining the parameters of the query which will be sent to the API in order to get a valid response with data
   params = { 
     'q': your_ingredients,
     'app_id' : api_id2,
@@ -29,8 +30,10 @@ def search_recipes(your_ingredients, cuisine_type = None, max_time = None, exclu
   if max_calories:
     params['calories'] = max_calories
 
-  response = requests.get(api_url2, params = params)
-
+  response = requests.get(api_url2, params = params) #making the request to the API using the parameters defined above and the second api url link
+  
+# checking if the request was successfull
+# return a json of the API response was successful, otherwise return None to avoid errors
   if response.status_code == 200:
     return response.json()
   else:

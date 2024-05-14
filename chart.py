6e34e0a94cd149659320d21efc79368be04aa46e
RemyTullii <166280992+RemyTullii@users.noreply.github.com>
2024-05-14 10:49:31 +0200

    Delete chart.py

--- chart.py
+++
@@ -1,4 +0,0 @@
-#chart_data = pd.DataFrame({'Nutrients': ['Proteins', 'Lipids', 'Carbs'], 'Macronutrients (g)': [protein, fats, carbs],'Details': [None, nutrients.get('FASAT').get('quantity')/servings, None]})
-#chart_data.set_index('Nutrients', inplace=True)
-#with st.expander("Show Nutrients"):
-#st.bar_chart(data=chart_data, y=['Macronutrients (g)', 'Details'])
